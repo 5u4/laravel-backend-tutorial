10869f573969d3c0343df307f0396690ed20ab53
Alex Wong <alexwongsenhung@gmail.com>
2018-06-02 17:32:35 -0700

    step 4 | post controller: index function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Resources\Post as PostResource;
+use Illuminate\Http\JsonResponse;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Get all the posts ordered by published date.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        /* get all posts ordered by published date */
+        $posts = Post::orderBy('date', 'desc')->get();
+
+        /* wrap posts in a resource */
+        return PostResource::collection($posts)->response();
+    }
 }
